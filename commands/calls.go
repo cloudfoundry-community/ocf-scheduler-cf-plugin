434eaf6b11f5c2944f29989ff8850353d2f6da83
Dennis Walters <dwalters@starkandwayne.com>
2022-05-09 15:49:32 -0400

    Aligned `cf calls` output with upstream

--- commands/calls.go
+++ commands/calls.go
@@ -2,6 +2,8 @@ package commands
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/starkandwayne/ocf-scheduler-cf-plugin/client"
 	"github.com/starkandwayne/ocf-scheduler-cf-plugin/core"
@@ -9,20 +11,46 @@ import (
 
 // cf calls
 func Calls(services *core.Services, args []string) {
+	if err := listCalls(services); err != nil {
+		fmt.Println("Error:", err.Error())
+	}
+
+	fmt.Println("OK")
+}
+
+func listCalls(services *core.Services) error {
+	err := core.PrintActionInProgress(services, "Listing calls")
+	if err != nil {
+		return err
+	}
+
 	space, err := core.MySpace(services)
 	if err != nil {
-		fmt.Println("Could not get current space.")
-		return
+		return fmt.Errorf("Could not get current space.")
+	}
+
+	apps, err := core.MyApps(services)
+	if err != nil {
+		return fmt.Errorf("Could not get apps.")
 	}
 
 	calls, err := client.ListCalls(services.Client, space)
 	if err != nil {
-		fmt.Printf("Could not get calls for space %s.\n", space.Name)
-		return
+		return fmt.Errorf("Could not get calls for space %s.\n", space.Name)
 	}
 
-	fmt.Printf("Calls for Space %s:\n\n", space.Name)
+	writer := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', uint(0))
+	fmt.Fprintln(writer, "Call Name\tApp Name\tURL")
+
 	for _, call := range calls {
-		fmt.Printf("\t%s (%s)\n", call.Name, call.GUID)
+		appName := "**UNKNOWN**"
+		if app, err := core.AppByGUID(apps, call.AppGUID); err == nil {
+			appName = app.Name
+		}
+
+		fmt.Fprintf(writer, "%s\t%s\t%s\n", call.Name, appName, call.URL)
 	}
+
+	writer.Flush()
+	return nil
 }
