a2f8dfdb15e10aecffdc4dd0a2619a549800e7c1
Dennis Walters <dwalters@starkandwayne.com>
2022-05-10 14:17:11 -0400

    Aligned call-schedules with upstream

--- commands/call-schedules.go
+++ commands/call-schedules.go
@@ -5,44 +5,102 @@ import (
 	"os"
 	"text/tabwriter"
 
+	scheduler "github.com/starkandwayne/scheduler-for-ocf/core"
+
 	"github.com/starkandwayne/ocf-scheduler-cf-plugin/client"
 	"github.com/starkandwayne/ocf-scheduler-cf-plugin/core"
 )
 
-// cf call-schedules CALL-NAME
+// cf call-schedules
 func CallSchedules(services *core.Services, args []string) {
-	if len(args) != 2 {
-		fmt.Println("cf call-schedules CALL-NAME")
+	if err := callSchedules(services); err != nil {
+		fmt.Println("Error:", err.Error())
 		return
 	}
 
+	fmt.Println("OK")
+}
+
+func callSchedules(services *core.Services) error {
 	space, err := core.MySpace(services)
 	if err != nil {
-		fmt.Println("Could not get current space.")
-		return
+		return fmt.Errorf("Could not get current space.")
 	}
 
-	name := args[1]
-
-	call, err := client.CallNamed(services.Client, space, name)
+	// BEGIN REAL IMPLEMENTATION
+	output := make(map[string][]string)
+	allApps, err := core.MyApps(services)
 	if err != nil {
-		fmt.Printf("Could not find call named %s in space %s.\n", name, space.Name)
-		return
+		return fmt.Errorf("Could not get apps.")
 	}
 
-	schedules, _ := client.ListCallSchedules(services.Client, call)
-	if len(schedules) == 0 {
-		fmt.Printf("No schedules for call %s.\n" + name)
-		return
+	appCalls := make(map[string][]*scheduler.Call)
+	allCalls, err := client.ListCalls(services.Client, space)
+	if err != nil {
+		return fmt.Errorf("Could not get calls for space %s.\n", space.Name)
 	}
 
-	writer := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', uint(0))
-	fmt.Fprintln(writer, "Call Name\tSchedule\tWhen")
-	fmt.Fprintln(writer, "=========\t========\t====")
-
-	for _, schedule := range schedules {
-		fmt.Fprintf(writer, "%s\t%s\t%s\n", call.Name, schedule.GUID, schedule.Expression)
+	err = core.PrintActionInProgress(services, "Getting scheduled calls")
+	if err != nil {
+		return err
 	}
 
-	writer.Flush()
+	for _, call := range allCalls {
+		if appCalls[call.AppGUID] == nil {
+			appCalls[call.AppGUID] = make([]*scheduler.Call, 0)
+		}
+
+		appCalls[call.AppGUID] = append(appCalls[call.AppGUID], call)
+	}
+
+	for appGUID, calls := range appCalls {
+		if len(calls) == 0 {
+			continue
+		}
+
+		for _, call := range calls {
+			schedules, serr := client.ListCallSchedules(services.Client, call)
+			if serr != nil {
+				continue
+			}
+
+			if len(schedules) == 0 {
+				continue
+			}
+
+			if output[appGUID] == nil {
+				output[appGUID] = []string{
+					"Call Name\tURL\tSchedule\tExpression",
+				}
+			}
+
+			for _, schedule := range schedules {
+				output[appGUID] = append(
+					output[appGUID],
+					fmt.Sprintf(
+						"%s\t%s\t%s\t%s",
+						call.Name,
+						call.URL,
+						schedule.GUID,
+						schedule.Expression,
+					),
+				)
+			}
+		}
+
+		app, err := core.AppByGUID(allApps, appGUID)
+		if err != nil {
+			continue
+		}
+
+		fmt.Printf("\nApp Name: %s\n", app.Name)
+
+		writer := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', uint(0))
+		for _, line := range output[appGUID] {
+			fmt.Fprintln(writer, line)
+		}
+		writer.Flush()
+	}
+
+	return nil
 }
